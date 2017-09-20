e04dbde37752bdbf386c28fec2f41bf932bc7772
momo1002 <momoko.nakada1002@gmail.com>
2017-09-20 11:06:37 -0700

    add assignment2

---
+++ Programmatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Programmatic
+//
+//  Created by Momoko Nakada on 2017-09-19.
+//  Copyright © 2017 Momoko Nakada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
